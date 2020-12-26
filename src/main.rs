7924ff33e761f0a792d7943374598337cf7da1c7
clux <sszynrae@gmail.com>
2020-12-26 18:05:47 +0000

    update comment

--- src/main.rs
+++ src/main.rs
@@ -702,8 +702,8 @@ fn release_packages<'m>(
 
             if pkg.config.registry().is_none() {
                 cargo::wait_for_publish(crate_name, &base.version_string, timeout, dry_run)?;
-                // give crates a little leeway to propagate a successful publish
-                // this should avoid failed to select failures where the next pkg in line depends on the previous
+                // HACK: Even once the index is updated, there seems to be another step before the publish is fully ready.
+                // We don't have a way yet to check for that, so waiting for now in hopes everything is ready
                 std::time::Duration::from_secs(5);
             } else {
                 log::debug!("Not waiting for publish because the registry is not crates.io and doesn't get updated automatically");
